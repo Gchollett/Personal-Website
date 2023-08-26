cb77be7d128836d383a3ec63503f646debe3ecec
garrett <garrettchollett@yahoo.com>
2023-08-26 00:32:14 -0500

    Added cookie popup

---
+++ src/components/cookie-popup.tsx
@@ -0,0 +1,34 @@
+import { FunctionComponent, useEffect, useState } from 'react';
+import 'reactjs-popup'
+import Popup from 'reactjs-popup'
+import 'reactjs-popup/dist/index.css'
+
+type Props = {
+    cookieConsent: string | null,
+}
+
+const CookiePopup : FunctionComponent<Props> = (props) => {
+    if(props.cookieConsent === null){
+        return (
+            <>
+                <Popup open modal>
+                    <>
+                        <div>
+                            Do you want cookies?
+                        </div>
+                        <div>
+                            <button onClick={()=>{
+                                localStorage.setItem('cookieConsent','true');
+                            }}>Yes!</button>
+                            <button onClick={() => {close()}}>No.</button>
+                        </div>
+                    </>
+                </Popup>
+            </>
+        )
+    } else {
+        return (<></>)
+    }
+}
+
+export default CookiePopup
