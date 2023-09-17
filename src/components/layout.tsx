02969447bd1a910c40c900dc68594f82ce53675d
garrett <garrettchollett@yahoo.com>
2023-09-17 17:19:16 -0500

    Miniscule changes

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,38 +1,23 @@
-import Link from "next/link";
 import { FunctionComponent, PropsWithChildren, useEffect, useState} from "react";
 import Head from "next/head";
 import Logo from "../resources/Logo.png"
-import DarkModeButton from "./dark-mode-button";
-import Menu from "./menu";
-import Title from "./title";
-import CookiePopup from "./cookie-popup";
 import Footer from "./footer";
+import Header from "./header";
 
 const Layout: FunctionComponent<PropsWithChildren> = ({children}) => {
-    const [darkModeEnabled, setDarkModeEnabled] = useState(true);
-    useEffect(() => {
-        if(localStorage.getItem('cookie-consent') !== 'accepted'){
-            setDarkModeEnabled(false);
-        }
-    })
     return (
-        <div>
+        <>
             <Head>
                 <title>Garrett's Website</title>
                 <link rel="icon" href={Logo.src}></link>
             </Head>
-            <CookiePopup darkMode={setDarkModeEnabled}/>
-            <div className="bar">
-                <Title/>
-                <Menu/>
-                {darkModeEnabled && <DarkModeButton/>}
-            </div>
+            <Header/>
             <div className="card">
                 {children}
             </div>
-            <div style={{paddingTop: "100px"}}></div>
+            <div style={{paddingTop: "110px"}}></div>
             <Footer/>
-        </div>
+        </>
     )
 }
 
