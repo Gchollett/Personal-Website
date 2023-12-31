7a853efcecad84fc393943cd4832ed14db5d83b8
garrett <garrettchollett@yahoo.com>
2023-12-31 00:16:33 -0600

    imporved vinyl roladex by using a map

--- src/pages/interests/records.tsx
+++ src/pages/interests/records.tsx
@@ -1,13 +1,15 @@
 import { NextPage } from "next";
 import Roladex, { RoladexItem } from "../../components/roladex";
 
-const FMacRumors = "https://upload.wikimedia.org/wikipedia/en/f/fb/FMacRumours.PNG"
-const FlowerBoy = "https://upload.wikimedia.org/wikipedia/en/c/c3/Tyler%2C_the_Creator_-_Flower_Boy.png"
-const CMIYGL = "https://img.buzzfeed.com/buzzfeed-static/complex/images/nji2i9pyf3ocnbccuvgc/tyler-the-creator-call-me-if-you-get-lost-album-cover-interview-lead-image.jpg?output-format=jpg&output-quality=auto"
-const MoM3 = 'https://upload.wikimedia.org/wikipedia/en/e/e2/Man_on_the_Moon_III.png'
-const MELH = 'https://upload.wikimedia.org/wikipedia/en/5/55/LaurynHillTheMiseducationofLaurynHillalbumcover.jpg'
-const BTI = 'https://upload.wikimedia.org/wikipedia/en/7/70/Childish-gambino-because-the-internet.gif'
-const TPAB = 'https://upload.wikimedia.org/wikipedia/en/f/f6/Kendrick_Lamar_-_To_Pimp_a_Butterfly.png'
+const vinyls = [
+    "https://upload.wikimedia.org/wikipedia/en/f/fb/FMacRumours.PNG", //Rumors
+    "https://upload.wikimedia.org/wikipedia/en/c/c3/Tyler%2C_the_Creator_-_Flower_Boy.png", //Flower Boy
+    "https://img.buzzfeed.com/buzzfeed-static/complex/images/nji2i9pyf3ocnbccuvgc/tyler-the-creator-call-me-if-you-get-lost-album-cover-interview-lead-image.jpg?output-format=jpg&output-quality=auto", //Call Me if You Get Lost
+    'https://upload.wikimedia.org/wikipedia/en/e/e2/Man_on_the_Moon_III.png', //Man on the Moon III
+    'https://upload.wikimedia.org/wikipedia/en/5/55/LaurynHillTheMiseducationofLaurynHillalbumcover.jpg', //The Miseducation of Lauryn Hill
+    'https://upload.wikimedia.org/wikipedia/en/7/70/Childish-gambino-because-the-internet.gif', //Because the Internet
+    'https://upload.wikimedia.org/wikipedia/en/f/f6/Kendrick_Lamar_-_To_Pimp_a_Butterfly.png' //To Pimp a Butterfly
+]
 const Records:NextPage = () => {
     return (
         <>
@@ -17,27 +19,7 @@ const Records:NextPage = () => {
                 Hover over them to pull them and have a look. Make sure to put them back afterwards. :)
             </p>
             <Roladex>
-                <RoladexItem>
-                    <img src={FMacRumors}></img>
-                </RoladexItem>
-                <RoladexItem>
-                    <img src={CMIYGL}></img>
-                </RoladexItem>
-                <RoladexItem>
-                    <img src={FlowerBoy}></img>
-                </RoladexItem>
-                <RoladexItem>
-                    <img src={MoM3}></img>
-                </RoladexItem>
-                <RoladexItem>
-                    <img src={MELH}></img>
-                </RoladexItem>
-                <RoladexItem>
-                    <img src={BTI}></img>
-                </RoladexItem>
-                <RoladexItem>
-                    <img src={TPAB}></img>
-                </RoladexItem>
+                {vinyls.map((record,i)=>{return(<RoladexItem key={i}><img key={i} src={record}/></RoladexItem>)})}
             </Roladex>
         </>
     )
