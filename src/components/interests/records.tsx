3f4ff35831652f36239fcac8d056823afe5ebb10
garrett <garrettchollett@yahoo.com>
2024-08-17 00:12:29 -0500

    Changed Record display and added white background to images

--- src/components/interests/records.tsx
+++ src/components/interests/records.tsx
@@ -1,19 +1,20 @@
-import Roladex, { RoladexItem } from "../../components/roladex";
 import useClient from "../../hooks/useClient";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const discogsToken = "VMRLDPYrGhJxnmJLQgQclUUszlVoxBsYZRpqowhb";
 
 const Records = () => {
     const [albums,setAlbums] = useState([])
     const client = useClient()
-    client.get(`https://api.discogs.com/users/Gmoney7896/collection/folders/0/releases?token=${discogsToken}`)
-    .then((response)=>{
-        setAlbums(response.data.releases.map((data : any)=>{return data.basic_information?.cover_image}))
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
+    useEffect(() => {
+        client.get(`https://api.discogs.com/users/Gmoney7896/collection/folders/0/releases?token=${discogsToken}&sort=artist`)
+        .then((response)=>{
+            setAlbums(response.data.releases.map((data : any)=>{return data.basic_information?.cover_image}))
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    },[])
     return (
         <div className="records-containter interest">
             <h1>Vinyl Collection</h1>
@@ -22,12 +23,12 @@ const Records = () => {
                     <p>
                         These are all of the Vinyl records I currently have in my collection.<br/>
                         I integrated with my Discogs account in order to update them automatically when I add a new recods to my account's collection.<br/>
-                        Hover over them to pull them and have a look. Make sure to put them back afterwards. :)
+                        This is just my current collection, you can always come and check for updates!
                     </p>
                 </div>
-                <Roladex>
-                    {albums.map((record,i)=>{return(<RoladexItem key={i}><img key={i} src={record}/></RoladexItem>)})}
-                </Roladex>
+                <div className="roladex">
+                    {albums.map((record,i)=>{return(<div className="roladex-item" key={i}><img key={i} src={record}/></div>)})}
+                </div>
             </div>
         </div>
     )
